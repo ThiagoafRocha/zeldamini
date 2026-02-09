195ccb512487e5b6ba6e6f24530ae002bc7491ac
Thiago Rocha <thiago_afr@homtail.com>
2026-02-09 14:07:53 -0300

    FEAT Adicionando inimigos

--- src/zeldamini/Spritesheet.java
+++ src/zeldamini/Spritesheet.java
@@ -11,6 +11,8 @@ public class Spritesheet {
 	public static BufferedImage[] player_front;
 	public static BufferedImage tileWall;
 	
+	public static BufferedImage[] enemy_front;
+	
 	public Spritesheet() {
 		try {
 			spritesheet = ImageIO.read(getClass().getResource("/spritesheet.png"));
@@ -19,8 +21,13 @@ public class Spritesheet {
 		}
 		
 		player_front = new BufferedImage[2];
+		enemy_front = new BufferedImage[1];
+		
 		player_front[0] = getSprite(0, 11, 16, 16);
 		player_front[1] = getSprite(16, 11, 16, 16);
+		
+		enemy_front[0] = getSprite(35, 232, 16,16);
+//		enemy_front[1] = getSprite(35, 232, 16,16);
 		
 		tileWall = getSprite(271, 219, 16, 16);
 	}
